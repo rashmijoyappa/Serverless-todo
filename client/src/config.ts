c25ea4c913abdb74106d22e4c8d25206ac032a68
Rashmi Joyappa <rashmik@rashmik-a01.vmware.com>
2021-06-17 17:50:37 +0530

    checkin files

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'jrmculquca'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'rashmijoyappa-dev.us.auth0.com',           // Auth0 domain
+  clientId: 'MFMkU4G4h3NYvxuNqFrButmvc7P6P4fw',  // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
